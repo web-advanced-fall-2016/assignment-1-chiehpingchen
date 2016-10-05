f88535de2cbb9642d4efeacd99f2e9c07cae0603
Che <chenc312@newschool.edu>
2016-10-05 17:31:03 -0400

    HW1_v1.3
    ohhhhh

--- scripts/main.js
+++ scripts/main.js
@@ -13,17 +13,17 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("slides");
+  var bottons = document.getElementsByClassName("botton");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < bottons.length; i++) {
+      bottons[i].className = bottons[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  bottons[slideIndex-1].className += " active";
 }
 
